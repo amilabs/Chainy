b5160fa5f856d0ac5c8120a287c7a93e7e59b631
ArtemKo7v <artemk@211.ru>
2015-04-06 22:18:42 +0700

    Notice fixed

--- app/classes/AmiLabs/Chainy/TX.php
+++ app/classes/AmiLabs/Chainy/TX.php
@@ -50,10 +50,11 @@ class TX extends \AmiLabs\CryptoKit\TX {
             return false;
         }
         $oRPC = new RPC();
+        $aResult = FALSE;
         try{
             $aResult = $oRPC->execCounterpartyd('get_block_info', array('block_index' => $block), false, true);
         }catch(\Exception $e){ /* todo */ }
-        return date('Y-m-d H:i:s', (int)$aResult['block_time']);
+        return is_array($aResult && isset($aResult['block_time'])) ? date('Y-m-d H:i:s', (int)$aResult['block_time']) : FALSE;
     }
     /**
      * Returns block and position inside a block by hash of the transaction.
