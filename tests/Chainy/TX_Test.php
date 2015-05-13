1a8cc9814d79b63ba547840cf957d1f2f162b79b
Deepeloper <weregod@gmail.com>
2015-05-13 13:42:05 +0700

    Changing unit tests structure.

--- tests/Chainy/TX_Test.php
+++ tests/Chainy/TX_Test.php
@@ -5,7 +5,7 @@ require_once 'config.php';
 
 use \AmiLabs\Chainy\TX;
 
-class Chainy_TXTest extends PHPUnit_Framework_TestCase{
+class TX_Test extends PHPUnit_Framework_TestCase{
     /**
      * @covers \AmiLabs\Chainy\TX::testGetBlockDate
      */
