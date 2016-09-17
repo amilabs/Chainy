156631b5181a2e19ea7d2939ba2e9c52240266d2
ArtemKo7v <artemk@211.ru>
2016-09-17 21:04:34 +0700

    Testnet support

--- app/templates/new.tpl.php
+++ app/templates/new.tpl.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+      <base href="http://chainy/web/">
 	<meta charset="utf-8" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
